4e7f0284a58a9b7514de826af7fc4be5495041ed
Carlos Castaño <11400.es@gmail.com>
2016-09-25 20:00:13 +0200

    Añadimos los estáticos de materialize Creamos los bloques y estructura de la web Mostramos los post en la HomeView

---
+++ blogs/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from blogs.views import HomeView
+
+urlpatterns = [
+    # url(r'^create$', PhotoCreationView.as_view(), name='photos_create'),
+    # url(r'^photos/$', PhotoListView.as_view(), name='photos_my_photos'),
+    # url(r'^photos/(?P<pk>[0-9]+)$', PhotoDetailView.as_view(), name='photos_detail'),
+    url(r'^$', HomeView.as_view(), name='blogs_home'),
+]
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from cuentame.settings import VISIBLE_SI
+from entradas.models import post
+
+
+class HomeView(View):
+
+    def get(self, request):
+        """
+        Renderiza el home con un listado de post visibles de todos los blogs
+        :param request: objeto HttpRequest con los datos de la petición
+        :return: objeto HttpResponse con los datos de la respuesta
+        """
+        # recupera todas los post visibles de la base de datos
+        posts = post.objects.filter(visible=VISIBLE_SI).order_by('-created_at')
+        context = {'posts_list': posts}
+        return render(request, 'entradas/inicio.html', context)

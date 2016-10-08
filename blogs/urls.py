ddea74a8db68c53ed0eb1c75773255a340dc26fd
Juan Carlos Castaño Moreno <11400.es@gmail.com>
2016-10-08 14:14:23 +0200

    Añadimos los blogs de un usuario Añadimos el detalle de un post con sus categorias Añadimos listado de post de una categoría

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
-from blogs.views import HomeView, BlogListView, BlogUserListView, BlogCatListView
+from blogs.views import HomeView, BlogListView, BlogUserListView, BlogCatListView, BlogUserDetailView
 
 urlpatterns = [
     # url(r'^create$', PhotoCreationView.as_view(), name='photos_create'),
     url(r'^blogs$', BlogListView.as_view(), name='blogs_list'),
-    url(r'^blogs/([\w-]+)/$', BlogUserListView.as_view(), name='blogsuser_list'),
-    url(r'^blogs/cat/(?P<pk>[\w-]+)/$', BlogCatListView.as_view(), name='blogscat_list'),
+    url(r'^blogs/(?P<username>[\w-]+)/$', BlogUserListView.as_view(), name='blogsuser_list'),
+    url(r'^blogs/(?P<username>[\w-]+)/(?P<postId>[0-9]+)$', BlogUserDetailView.as_view(), name='blogsuser_post'),
+    url(r'^blogs/cat/(?P<nombre>[\w-]+)/$', BlogCatListView.as_view(), name='blogscat_list'),
     url(r'^$', HomeView.as_view(), name='blogs_home'),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from rest_framework.generics import get_object_or_404
 
 from blogs.models import blogs, User
@@ -24,31 +26,89 @@ class HomeView(View):
         return render(request, 'entradas/inicio.html', context)
 
 class BlogListView(ListView):
-
+    """Renderizamos la lista de Blogs"""
     model = blogs
     context_object_name = 'blog_list'
     template_name = 'blogs/blog_list.html'
 
+    def get_context_data(self, **kwargs):
+        """Añadimos la categorias al contexto"""
+        context = super(BlogListView, self).get_context_data(**kwargs)
+        context['categoria_list'] = categorias.objects.all()
+        return context
+
     def get(self, request):
+        """Añadimos los blogs registrados al contexto"""
         result = super().get(request)
         return result
 
 class BlogUserListView(ListView):
-
+    """Renderizamos los post de un usuario"""
     model = post
     context_object_name = 'postuser_list'
     template_name = 'entradas/bloguser_list.html'
 
+    def get_context_data(self, **kwargs):
+        """Añadimos la categorias al contexto"""
+        context = super(BlogUserListView, self).get_context_data(**kwargs)
+        context['categoria_list'] = categorias.objects.all()
+        context['usuario'] = get_object_or_404(User, username=self.kwargs.get('username', None))
+        return context
+
     def get_queryset(self):
-        self.user = get_object_or_404(User, username=self.args[0])
-        return post.objects.filter(visible=VISIBLE_SI).order_by('-creado_el')
+        """Añadimos los post de un un usuario pasado por parametro username"""
+        self.user = get_object_or_404(User, username=self.kwargs.get('username', None))
+        return post.objects.filter(visible=VISIBLE_SI, blog__usuario=self.user).order_by('-creado_el')
 
 class BlogCatListView(ListView):
-
+    """Renderizamos los post de una determinada categoria"""
     model = post
     context_object_name = 'postcategorias_list'
     template_name = 'entradas/blogcat_list.html'
 
+    def get_context_data(self, **kwargs):
+        """Añadimos la categorias al contexto"""
+        context = super(BlogCatListView, self).get_context_data(**kwargs)
+        context['categoria_list'] = categorias.objects.all()
+        return context
+
     def get_queryset(self):
-        self.cat = get_object_or_404(categorias, nombre=self.args[0])
+        """Añadimos al contexto los posts de una determinada categoria, tomando el parametro nombre"""
+        self.cat = get_object_or_404(categorias, nombre=self.kwargs.get('nombre', None))
         return post.objects.filter(visible=VISIBLE_SI, cat=self.cat).order_by('-creado_el')
+
+class BlogUserDetailView(View):
+    """ Renderizamos el detalle de un post
+        parametros : categoria y usuario
+    """
+    # model = post
+    # context_object_name = 'post'
+    # template_name = 'entradas/blogcat_list.html'
+    #
+    # def get_context_data(self, **kwargs):
+    #     """Añadimos la categorias al contexto"""
+    #     context = super(BlogUserDetailView, self).get_context_data(**kwargs)
+    #     context['categoria_list'] = categorias.objects.all()
+    #     return context
+    #
+    # def get_queryset(self):
+    #     # self.cat = get_object_or_404(categorias, nombre=self.kwargs.get('nombre', None))
+    #     return post.objects.filter(visible=VISIBLE_SI, id=self.kwargs.get('postId', None))
+
+    def get(self, request, username, postId):
+        """
+        Renderiza el detalle de una imagen
+        :param request: objeto HttpRequest con los datos de la petición
+        :param pk: clave primaria de la foto a recuperar
+        :return: objeto HttpResponse con los datos de la respuesta
+        """
+        possible_post = post.objects.filter(id=postId)
+
+        if len(possible_post) == 0:
+            return HttpResponseNotFound("La imagen que buscas no existe")
+        elif len(possible_post) > 1:
+            return HttpResponse("Múltiples opciones", status=300)
+
+        post_select = possible_post[0]
+        context = {'post': post_select}
+        return render(request, 'entradas/bloguser_post.html', context)

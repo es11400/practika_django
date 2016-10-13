a1b289df413eb9c53057ba4064d2934c2e728d55
Juan Carlos Castaño Moreno <11400.es@gmail.com>
2016-10-13 13:06:30 +0200

    Añadimos el filtro de solo mostrar post menos de una fecha

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth import authenticate, login as django_login, logout as django_logout
-from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
-from django.utils.decorators import method_decorator
+from django.utils.datetime_safe import datetime
 from django.views import View
 from django.views.generic import ListView, DetailView
 from rest_framework.generics import get_object_or_404
@@ -27,7 +25,7 @@ class HomeView(Inc, View):
         :return: objeto HttpResponse con los datos de la respuesta
         """
         # recupera todas los post visibles de la base de datos
-        posts = post.objects.filter(visible=VISIBLE_SI).order_by('-creado_el')
+        posts = post.objects.filter(visible=VISIBLE_SI, fecha__lte=datetime.now()).order_by('-fecha')
         paginator = Paginator(posts, POSTxPAGINAS)
         page = request.GET.get('page')
         try:
@@ -97,7 +95,7 @@ class BlogPostListView(ListView):
     def get_queryset(self):
         """Añadimos los post de un blog pasado por parametro blogId"""
         self.blog = get_object_or_404(blogs, id=self.kwargs.get('blogId', None))
-        return post.objects.filter(visible=VISIBLE_SI, blog=self.blog).order_by('-creado_el')
+        return post.objects.filter(visible=VISIBLE_SI, fecha__lte=datetime.now(), blog=self.blog).order_by('-fecha')
 
     def post(self, request):
         return Inc.formularios(self, request)
@@ -128,7 +126,7 @@ class BlogUserListView(ListView):
     def get_queryset(self):
         """Añadimos los post de un un usuario pasado por parametro username"""
         self.user = get_object_or_404(User, username=self.kwargs.get('username', None))
-        return post.objects.filter(visible=VISIBLE_SI, blog__usuario=self.user).order_by('-creado_el')
+        return post.objects.filter(visible=VISIBLE_SI, fecha__lte=datetime.now(), blog__usuario=self.user).order_by('-fecha')
 
 class BlogCatListView(ListView):
     """Renderizamos los post de una determinada categoria"""
@@ -155,7 +153,7 @@ class BlogCatListView(ListView):
     def get_queryset(self):
         """Añadimos al contexto los posts de una determinada categoria, tomando el parametro nombre"""
         self.cat = get_object_or_404(categorias, nombre=self.kwargs.get('nombre', None))
-        return post.objects.filter(visible=VISIBLE_SI, cat=self.cat).order_by('-creado_el')
+        return post.objects.filter(visible=VISIBLE_SI, fecha__lte=datetime.now(), cat=self.cat).order_by('-fecha')
 
 class BlogCatUserListView(ListView):
     """Renderizamos los post un usuario de una determinada categoria"""
@@ -183,7 +181,7 @@ class BlogCatUserListView(ListView):
         """Añadimos al contexto los posts de un usuario y una categoria, tomando el parametro nombre de la categoria y username para el usuario"""
         self.cat = get_object_or_404(categorias, nombre=self.kwargs.get('nombre', None))
         self.user = get_object_or_404(User, username=self.kwargs.get('username', None))
-        return post.objects.filter(visible=VISIBLE_SI, blog__usuario= self.user, cat=self.cat).order_by('-creado_el')
+        return post.objects.filter(visible=VISIBLE_SI, fecha__lte=datetime.now(), blog__usuario= self.user, cat=self.cat).order_by('-fecha')
 
 class BlogUserDetailView(View):
     """ Renderizamos el detalle de un post
